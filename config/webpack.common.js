be27195a78a320dc256209d59e91ea32ed5d7844
wscart@qq.com <wscart@qq.com>
2021-04-04 21:37:28 +0800

    去掉image-webpack-loader插件(兼容问题)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -109,30 +109,6 @@ module.exports = {
                                 maxSize: imageInlineSizeLimit
                             }
                         },
-                        use: [
-                            {
-                                loader: 'image-webpack-loader',
-                                options: {
-                                    mozjpeg: {
-                                        progressive: true,
-                                        quality: 65,
-                                    },
-                                    optipng: {
-                                        enabled: false,
-                                    },
-                                    pngquant: {
-                                        quality: [0.65, 0.90],
-                                        speed: 4
-                                    },
-                                    gifsicle: {
-                                        interlaced: false,
-                                    },
-                                    webp: {
-                                        quality: 75,
-                                    },
-                                },
-                            },
-                        ],
                     },
                     {
                         test: /\.(eot|svg|ttf|woff|woff2?)$/,
--- typings.d.ts
+++ typings.d.ts
@@ -1,24 +1,11 @@
 // 这边申明了less文件 之后使用import styles from 'xxx.less'就不会有ts报错了
-declare module '*.png' {
-    const content: any;
-    export default content;
-}
-declare module '*.gif' {
-    const content: any;
-    export default content;
-}
-declare module '*.jpg' {
-    const content: any;
-    export default content;
-}
-declare module '*.jpeg' {
-    const content: any;
-    export default content;
-}
-declare module '*.svg' {
-    const content: any;
-    export default content;
-}
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
 declare module '*.css' {
     const content:{[className:string]:string};
     export default content;
