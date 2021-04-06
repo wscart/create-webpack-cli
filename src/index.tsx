f85f6570ae15a388f02329515e69c4281e4f30a7
wscart@qq.com <wscart@qq.com>
2021-04-06 16:28:04 +0800

    优化global.less

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import "normalize.css";
 import "./global.less";
 import xm from './assets/images/xm.jpeg';
 import styles from './index.less';
 
 import { getUserCurrentData } from '@src/services/app';
-
-
 
 const App = () => {
     useEffect(() => {
